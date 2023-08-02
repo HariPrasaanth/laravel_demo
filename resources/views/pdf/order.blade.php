b38e53b7d680c2cba5de32509d48b1328f913b17
unknown <hariprasaanth1998@gmail.com>
2023-08-02 22:45:02 +0530

    Japanese Characters in PDF

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -1,20 +1,40 @@
-<table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>S.No</th>
-        <th>Name</th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($fruits as $fruit)
-      <tr>
-        <td>{{$loop->iteration}}</td>
-        <td>{{ $fruit->name }}</td>
-        <td>
-          <a href="fruits/edit/{{$fruit->id}}">Edit</a>
-          <a href="fruits/delete/{{$fruit->id}}" onclick="return confirm('Are you sure you want to Delete?')">Delete</a>
-        </td>
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        @font-face {
+            font-family: 'NotoSansJP';
+            src: url('assets/fonts/NotoSansJP-Regular.ttf') format('truetype');
+        }
+
+        body {
+            font-family: 'NotoSansJP';
+            font-size: 12px;
+            overflow:visible;
+            white-space: pre-line;
+        }
+    </style>
+</head>
+
+<body>
+    <table style="width:100%;table-layout:fixed;">
+        <thead>
+            <tr>
+                <!-- <th>S.No</th> -->
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($fruits as $fruit)
+            <tr>
+                <!-- <td>{{$loop->iteration}}</td> -->
+                <td style="overflow-wrap: break-word">{{ $fruit->name }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
